d31bbb45be58ecc7b9c4ae93fd374fa3958e31d8
MuhammadRafiFarhan <96580397+MuhammadRafiFarhan@users.noreply.github.com>
2022-02-25 16:41:38 +0700

    Delete operator directory

--- operator/dist/javadoc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"operator","c":"FloatingPoint","l":"FloatingPoint()","url":"%3Cinit%3E()"},{"p":"operator","c":"FloatingPoint","l":"main(String[])","url":"main(java.lang.String[])"}]
