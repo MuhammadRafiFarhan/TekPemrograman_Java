63678ffc3615f02697010542c2d0ff6981213214
MuhammadRafiFarhan <96580397+MuhammadRafiFarhan@users.noreply.github.com>
2022-02-25 16:49:22 +0700

    Add files via upload

---
+++ Pertemuan 2 (W2)/operator/dist/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"operator","c":"FloatingPoint","l":"FloatingPoint()","url":"%3Cinit%3E()"},{"p":"operator","c":"FloatingPoint","l":"main(String[])","url":"main(java.lang.String[])"}]
