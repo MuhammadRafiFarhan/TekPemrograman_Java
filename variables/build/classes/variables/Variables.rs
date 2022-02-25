fd00a9b336fca126f593585000fe61c7979646d1
MuhammadRafiFarhan <96580397+MuhammadRafiFarhan@users.noreply.github.com>
2022-02-25 16:42:12 +0700

    Delete variables directory

--- variables/build/classes/variables/Variables.rs
+++
@@ -1,1 +0,0 @@
-variables.Constants
--- variables/dist/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"variables","l":"Constants"},{"p":"<Unnamed>","l":"Constants2"},{"p":"variables","l":"Constants2"}]
